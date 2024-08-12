5907fa57057d932a08ccec11e5fdec8d041aea9d
Dharneeshcr7 <76260467+Dharneeshcr7@users.noreply.github.com>
2024-08-12 12:57:07 +0530

    [ts-migrate][.] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -6,11 +6,9 @@ import { UserContext } from "../../context/UserContext";
 // import InfiniteScroll from 'react-infinite-scroll-component';
 import useBookSearch from "../../hooks/usePaginate";
 const List=()=>{
-    // const [items, setItems] = useState([]);
-    // const [isLoading, setIsLoading] = useState(false);
-    // //const [error, setError] = useState(null);
+    
     const [page, setPage] = useState(1);
-    // const [more,setMore]=useState(true);
+    
     const {
         books,
         hasMore,
@@ -30,39 +28,17 @@ const List=()=>{
         if (node) observer.current.observe(node)
       }, [loading, hasMore])
     
-      
-    // const fetchData=async ()=>{
-    //     setIsLoading(true);
-    //     try{     
-    //         const res=await axios.get(`https://customer-details-9dgv.onrender.com/people?_page=${page}`);
-    //         console.log(res.data)
-    //         setItems(prev=>[...prev,...res.data.data])
-    //         setPage(prev=>prev+1)
-            
-    //         setMore(res.data.data.length>0)
-    //     }
-    //     catch(err){
-    //        setError(err)
-    //     }
-    //     finally{
-    //         setIsLoading(false)
-    //     }
-    // }
-    // useEffect(() => {
-    //     fetchData();
-    //   }, []);
-
     
     return(
         <div className="max-h-[400px] overflow-y-auto border border-gray-300 p-4">
         
-        {books.map((book, index) => (
+        {books?books.map((book, index) => (
             <Card
-                key={book.id} // Use a unique key (assuming `id` is unique)
+                key={book.id} 
                 data={book}
                 ref={books.length === index + 1 ? lastBookElementRef : null}
             />
-        ))}
+        )):<h>Loading...</h>}
          
          
         </div>
